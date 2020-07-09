8612311632f2674f6e806ae306bf3cf5bed4d764
qq741263802 <741263802@qq.com>
2020-07-10 00:14:31 +0800

    新增Http工具类

---
+++ src/main/java/com/mmall/util/LbReptile.java
@@ -0,0 +1,8 @@
+package com.mmall.util;
+
+/**
+ * @author lhm
+ * @date 2020/7/9 22:17
+ */
+public class LbReptile {
+}
--- src/test/java/mmall/Loggertest.java
+++ src/test/java/mmall/Loggertest.java
@@ -2,7 +2,8 @@ package mmall;
 
 
 
-import com.mmall.common.ResponseCode;
+import com.mmall.util.http.HttpParamers;
+import com.mmall.util.http.HttpService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,8 +13,19 @@ private static Logger logger=LoggerFactory.getLogger(Loggertest.class);
 
     public static void main(String [] args)
     {
-      // System.out.println(ResponseCode.SUCCESS.getDesc());
-        logger.error("ceshi");
+
+        String uri = "http://jsonplaceholder.typicode.com";
+        String url="https://www.baidu.com/";
+        System.out.print("\n" + "test1---------------------------"+ "\n");
+        HttpParamers paramers = HttpParamers.httpGetParamers();
+        String response = "";
+        try {
+            HttpService httpService = new HttpService(url);
+            response = httpService.service("/", paramers);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.print(response);
 
 
     }
